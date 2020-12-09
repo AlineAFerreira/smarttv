edfea34a9accb9bcbc83e506c75e69ee44f294a1
Hugo Amaral <hugoamaralth@gmail.com>
2020-12-09 13:28:40 -0300

    update

--- src/pages/login.js
+++ src/pages/login.js
@@ -39,7 +39,7 @@ export default class Login extends React.Component {
 const LoginPage = styled.div`
     margin: 0;
     padding-left: 9.3vw;
-    background-image: #161819;
+    background-color: #161819;
     height:100vh;
     width:90.7vw;
     color: #CCC;
