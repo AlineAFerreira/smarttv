681c3e2a074b33d9f5f0421d7ba6dda90790b33b
AlineAFerreira <aline.a.ferreira@live.com>
2020-12-16 00:27:38 -0300

    Add Highlight component

--- src/App.js
+++ src/App.js
@@ -15,16 +15,7 @@ export default function App() {
       <GlobalStyle />
       <Router>
       <div>
-        <nav>
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/users">Users</Link>
-            </li>
-          </ul>
-        </nav>
+
 
         {/* A <Switch> looks through its children <Route>s and
             renders the first one that matches the current URL. */}
--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import { Container } from './styles';
+import Highlight from './../../components/Highlight';
 import News from './../../components/News';
 
   export default class Home extends React.Component {
     render() {
         return (
             <Container>
+                <Highlight />
                 <News highlight={true} />
             </Container>
         )
