d05d87a87cb3a2ba8bf739630f2e90b3c1383080
AlineAFerreira <aline.a.ferreira@live.com>
2021-02-01 23:10:20 -0300

    Change Procfile

---
+++ build/precache-manifest.b5d07cd28bda788699a8c8a0b9d9ca59.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c5b5b2e4ce8c2acb0d1e1f9fb4b98d67",
+    "url": "/smarttv/index.html"
+  },
+  {
+    "revision": "52d92f153277aecd787d",
+    "url": "/smarttv/static/css/main.49d386e9.chunk.css"
+  },
+  {
+    "revision": "ecae8503ea4cffc66693",
+    "url": "/smarttv/static/js/2.ccc7d01c.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/smarttv/static/js/2.ccc7d01c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "52d92f153277aecd787d",
+    "url": "/smarttv/static/js/main.36e1b189.chunk.js"
+  },
+  {
+    "revision": "a1027fd491350c318c6d",
+    "url": "/smarttv/static/js/runtime-main.124da6f8.js"
+  },
+  {
+    "revision": "65665f9a40412276570d71a02492b069",
+    "url": "/smarttv/static/media/SKY-Icon-Font.65665f9a.woff"
+  },
+  {
+    "revision": "ea3968a552efcaef92c0b4e0b73fef4c",
+    "url": "/smarttv/static/media/SKY-Icon-Font.ea3968a5.eot"
+  },
+  {
+    "revision": "fadcffe921ed0da6c891b67221fb45a0",
+    "url": "/smarttv/static/media/SKY-Icon-Font.fadcffe9.ttf"
+  },
+  {
+    "revision": "63f3054a824c9e047c2fc433ae36c3c9",
+    "url": "/smarttv/static/media/background-login.63f3054a.jpg"
+  },
+  {
+    "revision": "0b3af3d6034cd86d971aef0ccb7566bd",
+    "url": "/smarttv/static/media/logo-branco.0b3af3d6.png"
+  },
+  {
+    "revision": "0b3af3d6034cd86d971aef0ccb7566bd",
+    "url": "/smarttv/static/media/sky_logo-white.0b3af3d6.png"
+  }
+]);
